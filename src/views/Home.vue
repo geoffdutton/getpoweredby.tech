b1466649d0bb888f575c4304b0551daa3a58bce7
Geoff Dutton <g.dutton@gmail.com>
2018-07-06 22:17:30 -0500

    deploy to beta to get rolling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img src="../assets/splash-logo.jpg">
+        <HelloWorld msg="Welcome to GetPoweredBy.Tech"/>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
